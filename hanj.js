8ac54e198dfc29e287c9d2c60a273f083d6b81b0
WestKite <talk@kakao.one>
2022-06-16 14:53:43 +0900

    Add files via upload

--- hanj.js
+++ hanj.js
@@ -1,3 +1,7 @@
+var inject_footer = function(){
+	var footer_element = document.evaluate('//*[@id="footer"]/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+	footer_element.innerHTML = '<b style="color:#ff0000;">[CUSTOM MODE ENABLED]</b> &nbsp'+footer_element.innerText;
+};
 fetch('https://fo@rc@us.h@an@j@in.co.kr/login'.replaceAll('@',''))	//메인 JS파일이름 가져옴(자주 변경되기 때문에 유동적으로 적용)
 	.then(response=>response.text())
 	.then(codedata=>{
@@ -15,10 +19,8 @@ fetch('https://fo@rc@us.h@an@j@in.co.kr/login'.replaceAll('@',''))	//메인 JS�
 					.then(data1=>{
 						var convert_script = data1.replace("1e4",limit_value);		//10000개까지 조회 가능한걸 900000개로 수정
 						eval(convert_script);		//변경된 스크립트 로드
-				})
-		})
-	window.addEventListener('load', function(){
-		var footer_element = document.evaluate('//*[@id="footer"]/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-		footer_element.innerHTML = '<b style="color:#ff0000;">[CUSTOM MODE ENABLED]</b> &nbsp'+footer_element.innerText;
-	});
-})
+						if (document.readyState === "complete") inject_footer();
+						else (addEventListener || attachEvent).call(window, addEventListener ? "load" : "onload", inject_footer);
+					})
+			})
+	})
